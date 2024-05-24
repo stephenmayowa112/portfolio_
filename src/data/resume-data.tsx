b28d1bf37e93a81c665a33da177f49aaec1cf9d8
stephenmayowa112 <stephenmayowa112@gmail.com>
2024-05-24 08:24:37 +0100

    This is my minimalist portfolio website

---
+++ src/data/resume-data.tsx
@@ -0,0 +1,271 @@
+import {
+  AmbitLogo,
+  BarepapersLogo,
+  BimLogo,
+  CDGOLogo,
+  ClevertechLogo,
+  ConsultlyLogo,
+  EvercastLogo,
+  Howdy,
+  JarockiMeLogo,
+  JojoMobileLogo,
+  Minimal,
+  MobileVikingsLogo,
+  MonitoLogo,
+  NSNLogo,
+  ParabolLogo,
+  TastyCloudLogo,
+  YearProgressLogo,
+} from "@/images/logos";
+import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+
+export const RESUME_DATA = {
+  name: "Stephen Mayowa Ojunde",
+  initials: "SM",
+  location: "Lagos, Nigeria, WAT",
+  locationLink: "https://www.google.com/maps/place/lagos",
+  about:
+    "Full Stack Engineer focused on building products with extra attention to detail",
+  summary:
+    "Experienced web developer skilled in frontend  technologies, with proven track record of delivering user-friendly websites promptly and within budget. Actively seeking opportunities to innovate and advance digital solutions.",
+  avatarUrl: "https://res.cloudinary.com/dkkyvnhhg/image/upload/e_improve/v1716529165/miyagi_pic_ywcedk.png",
+  personalWebsiteUrl: "https://www.linkedin.com/in/stephenmayowa/",
+  contact: {
+    email: "stephenmayowa112@gmail.com",
+    tel: "+2348130864548",
+    social: [
+      {
+        name: "GitHub",
+        url: "https://github.com/stephenmayowa112",
+        icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/stephenmayowa/",
+        icon: LinkedInIcon,
+      },
+      {
+        name: "X",
+        url: "https://twitter.com/stephenmayowa16",
+        icon: XIcon,
+      },
+    ],
+  },
+  education: [
+    {
+      school: "University of Lagos",
+      degree: "Bachelor's Degree in Applied Physics",
+      start: "2017",
+      end: "2023",
+    },
+  ],
+  work: [
+    {
+      company: "Univelcity",
+      link: "https://parabol.co",
+      badges: ["Onsite"],
+      title: "Full Stack Developer (Trainee)",
+      logo: ParabolLogo,
+      start: "2024",
+      end: "Present",
+      description:
+        "Developed numerous full fledge responsive websites using react components, and django framework. \n Collaborated with designers and stakeholders to create wireframes, mockups. and prototypes for client projects. \n Managed version control using Git and collaborated with remote teams using Agile methodologies."
+    },
+    {
+      company: "Clevertech",
+      link: "https://clevertech.biz",
+      badges: ["Remote"],
+      title: "Lead Android Developer → Full Stack Developer",
+      logo: ClevertechLogo,
+      start: "2015",
+      end: "2021",
+      description:
+        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+    },
+    {
+      
+      company: "Nokia Siemens Networks",
+      link: "https://www.nokia.com",
+      badges: [],
+      title: "C/C++ Developer",
+      logo: NSNLogo,
+      start: "2010",
+      end: "2012",
+      description: "Creating and testing software for LTE base stations",
+    },
+  ],
+  skills: [
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Node.js",
+    "Python",
+    "Django",
+    "Git",
+    "Cloud Computing",
+    "Tailwind CSS",
+    
+  ],
+  projects: [
+    {
+      title: "Consultly",
+      techStack: [
+        "Side Project",
+        "TypeScript",
+        "Next.js",
+        "Vite",
+        "GraphQL",
+        "WebRTC",
+      ],
+      description: "A platform to build and grow your online business",
+      logo: ConsultlyLogo,
+      link: {
+        label: "consultly.com",
+        href: "https://consultly.com/",
+      },
+    },
+    {
+      title: "Monito",
+      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      description:
+        "Browser extension that records everything happening in a web application",
+      logo: MonitoLogo,
+      link: {
+        label: "monito.dev",
+        href: "https://monito.dev/",
+      },
+    },
+    {
+      title: "Jarocki.me",
+      techStack: ["Side Project", "Next.js", "MDX"],
+      description:
+        "My personal website and blog. Built with Next.js and Notion API",
+      logo: JarockiMeLogo,
+      link: {
+        label: "github.com",
+        href: "https://jarocki.me/",
+      },
+    },
+    {
+      title: "Minimal",
+      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      description:
+        "Minimalist calendars, habit trackers and planners generator",
+      logo: Minimal,
+      link: {
+        label: "useminimal.com",
+        href: "https://useminimal.com/",
+      },
+    },
+    {
+      title: "Barepapers",
+      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      description:
+        "Generates beautiful wallpapers using random shapes and gradients",
+      logo: BarepapersLogo,
+      link: {
+        label: "barepapers.com",
+        href: "https://barepapers.com/",
+      },
+    },
+    {
+      title: "Year progress",
+      techStack: ["Side Project", "TypeScript", "Next.js"],
+      description: "Tracks current year progress and displays a countdown",
+      logo: YearProgressLogo,
+      link: {
+        label: "getyearprogress.com",
+        href: "https://getyearprogress.com/",
+      },
+    },
+    {
+      title: "Parabol",
+      techStack: [
+        "Full Stack Developer",
+        "TypeScript",
+        "React",
+        "Node.js",
+        "GraphQL",
+      ],
+      description:
+        "The Agile meeting co-pilot that delivers better meetings with less effort",
+      logo: ParabolLogo,
+      link: {
+        label: "github.com",
+        href: "https://parabol.co/",
+      },
+    },
+    {
+      title: "Evercast",
+      techStack: [
+        "Lead Frontend Developer",
+
+        "TypeScript",
+        "React",
+        "Node.js",
+        "GraphQL",
+      ],
+      description:
+        "Creative collaboration platform that combines video conferencing and HD media streaming",
+      logo: EvercastLogo,
+      link: {
+        label: "evercast.us",
+        href: "https://www.evercast.us/",
+      },
+    },
+    {
+      title: "Mobile Vikings",
+      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      description:
+        "Android application for leading virtual mobile operator in Poland",
+      logo: MobileVikingsLogo,
+      link: {
+        label: "mobilevikings.pl",
+        href: "https://mobilevikings.pl/",
+      },
+    },
+    {
+      title: "Howdy",
+      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      description:
+        "Howdy is a place for you to join communities you care about",
+      logo: Howdy,
+      link: {
+        label: "play.google.com",
+        href: "https://howdy.co/",
+      },
+    },
+    {
+      title: "Tastycloud",
+      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      description:
+        "Android application for managing and displaying restaurant menus in kiosk mode",
+      logo: TastyCloudLogo,
+      link: {
+        label: "tastycloud.fr",
+        href: "https://www.tastycloud.fr/",
+      },
+    },
+    {
+      title: "Ambit",
+      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      description:
+        "Android application that helps with sharing your contact details",
+      logo: AmbitLogo,
+    },
+    {
+      title: "Bim",
+      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      description:
+        "Android application that helps with booking a table in a restaurants",
+      logo: BimLogo,
+    },
+    {
+      title: "Canal Digital GO",
+      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      description:
+        "Video streaming mobile application for Canal Digital subscribers",
+      logo: CDGOLogo,
+    },
+  ],
+} as const;
